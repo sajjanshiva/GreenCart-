1ae85b1b5ec23e8492e1cca97d94696081c5e289
shiva <shivamani8496@gmail.com>
2025-09-16 01:14:11 +0530

    Updated frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import BestSeller from '@/components/bestSeller'
+
+import BestSeller from '@/components/BestSeller'
 import BottomBanner from '@/components/BottomBanner'
 import Categories from '@/components/Categories'
 import MainBanner from '@/components/MainBanner'
