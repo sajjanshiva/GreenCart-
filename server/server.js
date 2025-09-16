4ca437b6d53493fdad8077c9d596fa0f816623b9
shiva <shivamani8496@gmail.com>
2025-09-16 17:29:10 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -28,17 +28,7 @@ const allowedOrigins = [
 ];
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://greencart-shiva4.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
+app.use(cors({origin: allowedOrigins , credentials: true}));
 
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks );
